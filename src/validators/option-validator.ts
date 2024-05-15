799e04594ebc738bae0960d8422c8fb6b4e9c51b
michalrudy9 <michallrudy9@gmail.com>
2024-05-15 22:40:34 +0200

    Added the next customise case abilities

--- src/validators/option-validator.ts
+++ src/validators/option-validator.ts
@@ -2,6 +2,8 @@
 // bg-zinc-900 border-zinc-900
 // bg-rose-950 border-rose-950
 
+import { PRODUCT_PRICES } from "@/config/products";
+
 export const COLORS = [
   { label: "Black", value: "balck", tw: "zinc-900" },
   { label: "Blue", value: "blue", tw: "blue-950" },
@@ -17,5 +19,41 @@ export const MODELS = {
     { label: "iPhone 13", value: "iphone13" },
     { label: "iPhone 14", value: "iphone14" },
     { label: "iPhone 15", value: "iphone15" },
+  ],
+} as const;
+
+export const MATERIALS = {
+  name: "material",
+  options: [
+    {
+      label: "Silicone",
+      value: "silicone",
+      description: undefined,
+      price: PRODUCT_PRICES.material.silicone,
+    },
+    {
+      label: "Soft Polycarbonate",
+      value: "polycarbonate",
+      description: "Scratch-resistant coating",
+      price: PRODUCT_PRICES.material.polycarbonate,
+    },
+  ],
+} as const;
+
+export const FINISHES = {
+  name: "finish",
+  options: [
+    {
+      label: "Smooth Finish",
+      value: "smooth",
+      description: undefined,
+      price: PRODUCT_PRICES.finish.smooth,
+    },
+    {
+      label: "Textured Finish",
+      value: "textured",
+      description: "Soft grippy texture",
+      price: PRODUCT_PRICES.finish.textured,
+    },
   ],
 } as const;
