33540f32388807ef59f1631bbe083cb6e3d0fc2c
michalrudy9 <michallrudy9@gmail.com>
2024-05-25 15:04:39 +0200

    Fixed a typo

--- src/validators/option-validator.ts
+++ src/validators/option-validator.ts
@@ -5,7 +5,7 @@
 import { PRODUCT_PRICES } from "@/config/products";
 
 export const COLORS = [
-  { label: "Black", value: "balck", tw: "zinc-900" },
+  { label: "Black", value: "black", tw: "zinc-900" },
   { label: "Blue", value: "blue", tw: "blue-950" },
   { label: "Rose", value: "rose", tw: "rose-950" },
 ] as const;
