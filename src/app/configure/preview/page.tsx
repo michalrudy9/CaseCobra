59418be09d190d61c149b4902ef2b1f291e2956b
michalrudy9 <michallrudy9@gmail.com>
2024-05-26 15:32:53 +0200

    Added page for preview path

---
+++ src/app/configure/preview/page.tsx
@@ -0,0 +1,29 @@
+import { db } from "@/db";
+import { notFound } from "next/navigation";
+import DesignPreview from "./DesignPreview";
+
+interface PageProps {
+  serachParams: {
+    [key: string]: string | string[] | undefined;
+  };
+}
+
+const Page = async ({ searchParams }: PageProps) => {
+  const { id } = searchParams;
+
+  if (!id || typeof id !== "string") {
+    return notFound();
+  }
+
+  const confuguration = await db.configuration.findUnique({
+    where: { id },
+  });
+
+  if (!confuguration) {
+    notFound();
+  }
+
+  return <DesignPreview configuration={confuguration} />;
+};
+
+export default Page;
