f5ed9b7c1e838af1caef92b069359d52f7945177
michalrudy9 <michallrudy9@gmail.com>
2024-06-01 21:06:24 +0200

    Corrected spelling

--- src/app/configure/preview/page.tsx
+++ src/app/configure/preview/page.tsx
@@ -3,7 +3,7 @@ import { notFound } from "next/navigation";
 import DesignPreview from "./DesignPreview";
 
 interface PageProps {
-  serachParams: {
+  searchParams: {
     [key: string]: string | string[] | undefined;
   };
 }
